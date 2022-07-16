861197d0299285a1b048d82b661ddb4485587797
praveen <pkumarprajapati007@gmail.com>
2022-07-17 00:50:24 +0530

    refactoring and code improvements

---
+++ src/app/datalayer/brands.ts
@@ -0,0 +1,56 @@
+import { Brand } from "../models/types/brand";
+
+export const getBrandsList = (): Brand[] => ([
+    {
+        id: "honda",
+        name: "Honda",
+        img: "assets/images/Honda-Logo.jpg"
+    },
+    {
+        id: "Yamaha",
+        name: "Yamaha",
+        img: "assets/images/yamaha.png"
+    },
+
+    {
+        id: "tvs",
+        name: "TVS",
+        img: "assets/images/TVS.png"
+    },
+
+    {
+        id: "royal",
+        name: "Royal",
+        img: "assets/images/Royal-logo.png"
+    },
+
+    {
+        id: "hero",
+        name: "Hero",
+        img: "assets/images/Hero-Logo.png"
+    },
+
+    {
+        id: "suzuki",
+        name: "Suzuki",
+        img: "assets/images/suzuki.png"
+    },
+
+    {
+        id: "bajaj",
+        name: "Bajaj",
+        img: "assets/images/bajaj.png",
+    },
+
+    {
+        id: "ktm",
+        name: "KTM",
+        img: "assets/images/ktm.jpg",
+    },
+
+    {
+        id: "bmw",
+        name: "BMW",
+        img: "assets/images/ktm.jpg",
+    },
+])
