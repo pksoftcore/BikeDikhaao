a5c8d6a2723eb07335d2a143e9fe20a7b9d12a49
praveen <pkumarprajapati007@gmail.com>
2022-07-24 20:48:41 +0530

    Added footer

---
+++ src/app/datalayer/in-memory/sample-data/info.ts
@@ -0,0 +1,16 @@
+import { githubLogo, linkedinLogo } from "src/assets";
+
+const myLinkedInUrl = 'https://www.linkedin.com/in/praveen-prajapati-17b69b196/';
+
+const myGithubUrl = 'https://github.com/praveenkumarprajapati/praveenkumarprajapati';
+
+export const infoData = [
+    {
+      logo: linkedinLogo,
+      redirect: myLinkedInUrl
+    },
+    {
+      logo: githubLogo,
+      redirect: myGithubUrl
+    }
+  ]
